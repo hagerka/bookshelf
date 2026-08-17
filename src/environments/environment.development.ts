bca057247e5203350cf3533a83728df67a79f42f
Kimberly Hager <hager.kimberly.a@gmail.com>
2026-08-16 21:29:16 -0400

    updates appUrl

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,6 @@
 export const environment = {
     production: false,
+    appUrl: 'https://hagerka.github.io/bookshelf',
     supabaseUrl: 'https://wfdhusxpevqkhxnwbmnr.supabase.co',
     supabaseKey: 'sb_publishable_v_68qIxe3MaEn3sv3Ul-yQ_NtpQeOvX',
 };
